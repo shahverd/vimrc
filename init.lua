61491d4a3a4cd6ca6c53329271acdfd54e6c81da
Mostafa Shahverdy <shahverdy.m@gmail.com>
2025-11-04 15:19:19 +0330

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,95 +5,29 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.signcolumn = 'yes:2'
---vim.opt.winborder = 'single'
--- vim.cmd.colorscheme 'delek'
+vim.opt.signcolumn = 'yes:1'
+vim.g.netrw_liststyle = 3
 vim.cmd.colorscheme 'unokai'
 
-vim.g.netrw_banner = 0
-vim.g.netrw_liststyle = 3
-vim.g.netrw_winsize = 20
-vim.g.netrw_browse_split = 4
---vim.g.netrw_altv = 1
-
--- Toggle Vexplore with <Leader>e
-vim.keymap.set('n', '<Leader>e', ":NvimTreeToggle<CR>")
-
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>e', function()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_get_option(buf, 'filetype') == 'netrw' then
+      vim.cmd("Rex")
+      return 
+    end
+  end
+  vim.cmd("Explore")
+end)
 
 --------------------------PLUGINS-------------------------------
-
 -- Enable LSP for C
-vim.lsp.enable('clangd', {
-  filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
-  -- Optional: Add settings specific to clangd
-  settings = {
-    clangd = {
-      -- Example: Set the compilation database path if needed
-      arguments = {
-        '--compile-commands-dir=build'
-      },
-    }
-  }
-})
-
--- Show error/warning message of LSP
-vim.keymap.set('n', '<leader>d', 
-  vim.diagnostic.open_float, 
-  {
-    desc = "Show full diagnostic message"
-  }
-)
+vim.lsp.enable('clangd', { filetypes = { 'c', 'objc'}, })
 
 -- Enable better code completion
-local cmp = require('cmp')
-cmp.setup({
-  -- The list of completion sources to use
+require('cmp').setup({
   sources = {
-    -- LSP suggestions (from cmp-nvim-lsp)
     { name = 'nvim_lsp' },
-    
-    -- Current buffer text 
-    -- (from cmp-buffer, often another required plugin)
     { name = 'buffer' }, 
-  },
-
-  -- Keymappings for the completion menu
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(), 
-    ['<C-e>'] = cmp.mapping.abort(), 
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), 
-  }),
-
-  -- Appearance and other settings
-  completion = {
-    keyword_length = 1, -- Start completion after 1 character
-  },
-})
-
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
-
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
   },
 })
