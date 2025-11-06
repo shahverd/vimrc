a961bd205c8929657022381b745ba31af063618f
Mostafa Shahverdy <shahverdy.m@gmail.com>
2025-11-06 05:46:53 +0330

    fixed issue with list stle in netrw

--- init.lua
+++ init.lua
@@ -6,12 +6,19 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.signcolumn = 'yes:1'
-vim.g.netrw_liststyle = 3
 vim.cmd.colorscheme 'unokai'
 
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>e', function() 
   vim.cmd(vim.fn.exists(':Rexplore') == 1 and 'Rexplore' or 'Explore') end)
+
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.g.netrw_liststyle = 3
+    vim.g.netrw_browse_split = 0
+  end,
+})
 
 --------------PLUGINS-------------
 -- Enable LSP for C
