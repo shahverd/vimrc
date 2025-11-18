c2686acaee5a0049e248167de367ed3dfcfafe4d
Mostafa Shahverdy <shahverdy.m@gmail.com>
2025-11-18 20:33:55 +0330

    temp autocomplete

--- init.lua
+++ init.lua
@@ -15,6 +15,7 @@ require("lsp")
 require("autocomplete")
 local gdb = require("gdb")
 
+
 --================KEYMAPS=================--
 
 -- Window movement in normal mode
@@ -27,6 +28,24 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true })
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true }) 
 vim.keymap.set('n', '<leader>t',  ":vsplit | terminal<CR>")
 
+vim.keymap.set('n', '<leader>f',  "<Right>vf,")
+
+vim.keymap.set('i', '<CR>',  function()
+  if vim.fn.pumvisible() == 1 then
+    return "<C-y>"
+  else
+    return "<CR>"
+  end
+end,{expr = true, noremap = true})
+
+vim.keymap.set('i', '<Tab>',  function()
+  if vim.fn.pumvisible() == 1 then
+    return "<C-n>"
+  else
+    return "<Tab>"
+  end
+end,{expr = true, noremap = true})
+
 vim.keymap.set('n', 'gd',         vim.lsp.buf.definition)
 vim.keymap.set('n', '[d',         vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d',         vim.diagnostic.goto_next)
@@ -34,7 +53,6 @@ vim.keymap.set('n', ']d',         vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>k',  vim.lsp.buf.hover)
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
-vim.keymap.set('n', '<leader>v',  vim.diagnostic.open_float)
 
 vim.keymap.set( 'n', '<leader>dc', ":lua require('gdb').start('./build/MyProject')<CR>", {noremap=true, silent=true})
 vim.keymap.set( 'n', '<leader>dq', ":lua require('gdb').stop()<CR>", {noremap=true, silent=true})
